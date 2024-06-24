1d42eaf2d37491b659337a10d03bc2aa23687b6e
Leslie-L <leslie.centeno@galileo.edu>
2024-06-23 20:27:22 -0600

    clase 9,10 y 11

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,42 +3,43 @@
 import Header from './Header.vue'
 import Layout from './Layout.vue'
 import Resume from './Resume/Index.vue'
-import Movements from './Movements/Index.vue' 
+import Movements from './Movements/Index.vue'
+import Action from './Action.vue'
 
-const movements =  [
-        {
-          id: 1,
-          title: "Movimiento",
-          description: "Deposito de salario",
-          amount: "1000",
-        },
-        {
-          id: 2,
-          title: "Movimiento 1",
-          description: "Deposito de honorarios",
-          amount: "500",
-        },
-        {
-          id: 3,
-          title: "Movimiento 3",
-          description: "Comida",
-          amount: "-100",
-        },
-        {
-          id: 4,
-          title: "Movimiento 4",
-          description: "Colegiatura",
-          amount: "1000",
-        },
-        {
-          id: 5,
-          title: "Movimiento 5",
-          description: "Reparación equipo",
-          amount: "1000",
-        },
-      ]
+const movements = [
+  {
+    id: 1,
+    title: 'Movimiento',
+    description: 'Deposito de salario',
+    amount: 1000
+  },
+  {
+    id: 2,
+    title: 'Movimiento 1',
+    description: 'Deposito de honorarios',
+    amount: 500
+  },
+  {
+    id: 3,
+    title: 'Movimiento 3',
+    description: 'Comida',
+    amount: -100
+  },
+  {
+    id: 4,
+    title: 'Movimiento 4',
+    description: 'Colegiatura',
+    amount: 1000
+  },
+  {
+    id: 5,
+    title: 'Movimiento 5',
+    description: 'Reparación equipo',
+    amount: 1000
+  }
+]
 
-const amount=null;
+const amount = null
 </script>
 <template>
   <Layout>
@@ -46,20 +47,15 @@ const amount=null;
       <Header />
     </template>
     <template #resume>
-      <Resume 
-        :label="'Ahorro total'"
-        :total-amount="100000"
-        :amount="amount"
-      >
-      <template #grapic>graphic</template>
-      <template #action>action</template>
+      <Resume :label="'Ahorro total'" :total-amount="100000" :amount="amount">
+        <template #grapic>graphic</template>
+        <template #action>
+          <Action />
+        </template>
       </Resume>
     </template>
     <template #movements>
-      <Movements
-        :movements="movements"
-      
-      />
+      <Movements :movements="movements" />
     </template>
   </Layout>
 </template>
